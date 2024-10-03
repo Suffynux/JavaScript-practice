36a6d616e9fd7bbf7864dbf903dbd4985889f09f
suffynux <suffynux@gmail.com>
2024-10-03 11:01:01 +0500

    Started files with Udemy Video Series

---
+++ Array.js
@@ -0,0 +1,6 @@
+// Merge two arrays
+let FavoritesCities = ["Berlin", "Paris", "Islamabad"]
+let visitedCities = ['Rawalpindi' , 'Mirpur']
+
+let mergeCities = concatFavoritesCities + visitedCities
+console.log(typeof mergeCities);
