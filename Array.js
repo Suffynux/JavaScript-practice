7993cc8e6958c33508eae379dca9db0b754dd093
suffynux <suffynux@gmail.com>
2024-10-10 19:41:50 +0500

    Array excercises done

--- Array.js
+++ Array.js
@@ -15,3 +15,38 @@ let isMadinaInList = citylist.includes("Madina")
 // console.log(isMadinaInList)
 
 
+// Array Tasks 
+// Task 1: Array Filtering
+function filterNumbers(arr) {
+    const result = arr.filter((element) => {
+        return typeof element === 'number' 
+    });
+    return result;
+}
+
+// Task 2: Array Reversal
+function reverseArray(arr) {
+    let result = arr.reverse()
+  return result;
+}
+
+// Task 3: Find Maximum in an Array
+function findMax(arr) {
+    let result = Math.max(...arr)
+  return result;
+}
+
+// Task 4: Remove Duplicates from an Array
+function removeDuplicates(arr) {
+    let uniqueValues = [...new Set(arr)]
+  return uniqueValues;
+}
+
+// Task 5: Flatten a Nested Array
+function flattenArray(arr) {
+    return arr.flat(Infinity);
+}
+
+
+
+
