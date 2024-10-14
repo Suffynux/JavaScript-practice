9b5c61069897ada3ab5471af19f2313bcd0137b2
suffynux <suffynux@gmail.com>
2024-10-14 14:11:50 +0500

    Excercises done with the help of DOM

--- DOM.JS/index.js
+++ DOM.JS/index.js
@@ -1,1 +1,7 @@
-document.getElementsByTagName("h1").length; 
+// Example 1 Solution
+// Step 1 : Grab the event listener
+document.getElementById("changeTextButton").
+addEventListener('click', function(){
+    let paragraph = (document.getElementById("myparagraph"))
+    paragraph.textContent = "Paragraph is changed with the help of the DOM"
+})
