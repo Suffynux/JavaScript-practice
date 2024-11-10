272276cd89fccf6d64a9c78f8a7f9375b6064216
suffynux <suffynux@gmail.com>
2024-11-10 11:00:32 +0500

    done

--- Projects/Weather_APP/index.js
+++ Projects/Weather_APP/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
+
     // Grabbing all the elements
     const weatherInput = document.getElementById("weatherInput");
     const getWeatherBtn = document.getElementById("weatherBtn");
@@ -10,9 +11,48 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const errorDisplay = document.getElementById("errorMessage");
 
 
+    // My API key
+    const API_KEY = "2c4b02009c115be329cf2a2baf25fc1c";
+
+
     // Adding event listener to the button 
     getWeatherBtnc.addEventListener("click" , () => {
-        weatherInput.value.trim
+        let city = weatherInput.value.trim();
+        if (!city) return;
+        // Alway remember when make requests on the other servers from Sir hitesh
+        // Data will take some times
+        // It may throw some errors 
+        // Servers/database always in the other country 
+    
+        try {
+            fetcWeatherData(city)
+        } catch (error) {
+            showError
+        }
+
+
     });
+
+
+    // Creating functions for Fetch data and display data
+    function fetcWeatherData(city) {
+
+
+    };
+
+    function displayWeatherData (weatherData) {
+
+    };
+
+
+    // In the below function we add the hidden to the weather Info data in case we show the error and remove the hidden class from the error message in case of error 
+    function showError() {
+        weatherInfo.classList.add('hidden');
+        errorDisplay.classList.remove('hidden');
+
+    }
+
+
+
 
 })
