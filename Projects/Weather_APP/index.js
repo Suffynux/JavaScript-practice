eefc2a011a9ef453c2233689878e0a2a3d412d93
suffynux <suffynux@gmail.com>
2024-11-10 10:42:02 +0500

    Front end designing

---
+++ Projects/Weather_APP/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    // Grabbing all the elements
+    const weatherInput = document.getElementById("weatherInput");
+    const getWeatherBtn = document.getElementById("weatherBtn");
+    const weatherInfo = document.getElementById("weatherInfo");
+    const cityNameDisplay = document.getElementById("cityName");
+    const temperatureDisplay = document.getElementById("temperature");
+    const descriptionDisplay = document.getElementById("description");
+    const errorDisplay = document.getElementById("errorMessage");
+
+
+    // Adding event listener to the button 
+    getWeatherBtnc.addEventListener("click" , () => {
+        weatherInput.value.trim
+    });
+
+})
