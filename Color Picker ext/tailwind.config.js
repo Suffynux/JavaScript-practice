1f4463f0a3dc8abb5d013d1e4de6cac0b5c97c2d
Sufiyan <suffynux@gmail.com>
2025-01-11 01:40:37 +0500

    Project completed

---
+++ Color Picker ext/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*.html", // Include all HTML files in the current directory
+    "./*.js",   // Include all JS files in the current directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
